5beef08c18ec1437ac50058795b21ff6ed046dbf
Olga <panda146@yandex.ru>
2023-06-21 13:15:11 +0300

    1

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Это очень корявое решение
+Console.WriteLine(" Введите пятизначное число");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+int b1 = a1 / 10000;
+int c1 = a1 % 10;
+int b2 = a1 / 1000 % 10;
+int c2 = a1 / 10 % 10;
+
+if (b1 == c1 && b2 == c2)
+{
+    Console.WriteLine("Это палиндром");
+}
+else
+{
+    Console.WriteLine("Это не палиндром");
+}
+
